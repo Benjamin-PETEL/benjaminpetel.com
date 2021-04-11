72e40685a70b237b951ae6f0516129c015ba32cc
Benjamin PETEL <benjamin.petel@outlook.com>
2021-04-11 22:43:35 +0200

    Correction boutons + logo competences transverses

--- js/competences.js
+++ js/competences.js
@@ -8,29 +8,30 @@ buttonJava.addEventListener("click", function(){displayCompetence("java")});
 buttonBack.addEventListener("click",function(){displayCompetence("backend")});
 buttonFront.addEventListener("click",function(){displayCompetence("frontend")});
 
+
 function displayCompetence(competence){
     document.getElementById("transverse").style.display = "none";
     document.getElementById("backend").style.display = "none";
     document.getElementById("java").style.display = "none";    
     document.getElementById("frontend").style.display = "none";
-    document.getElementById("button_transverse").style.backgroundColor = "#555";
-    document.getElementById("button_java").style.backgroundColor = "#555";
-    document.getElementById("button_back").style.backgroundColor = "#555";
-    document.getElementById("button_front").style.backgroundColor = "#555"; 
+    document.getElementById("button_transverse").style.backgroundColor = "#fff";
+    document.getElementById("button_java").style.backgroundColor = "#fff";
+    document.getElementById("button_back").style.backgroundColor = "#fff";
+    document.getElementById("button_front").style.backgroundColor = "#fff"; 
     if (competence=="transverse"){
         document.getElementById("transverse").style.display = "block";
-        document.getElementById("button_transverse").style.backgroundColor = "#777";
+        document.getElementById("button_transverse").style.backgroundColor = "#555";
     }
     if (competence=="java"){
         document.getElementById("java").style.display = "block";
-        document.getElementById("button_java").style.backgroundColor = "#777";
+        document.getElementById("button_java").style.backgroundColor = "#555";
     }
     if (competence=="frontend"){
         document.getElementById("frontend").style.display = "block";
-        document.getElementById("button_front").style.backgroundColor = "#777";
+        document.getElementById("button_front").style.backgroundColor = "#555";
     }
     if(competence=="backend"){
         document.getElementById("backend").style.display = "block";
-        document.getElementById("button_back").style.backgroundColor = "#777";
+        document.getElementById("button_back").style.backgroundColor = "#555";
     }
 }
