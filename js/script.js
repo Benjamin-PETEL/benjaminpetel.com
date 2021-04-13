aeca681c71e1503f6cc88b90ade370e7656fa7c7
Benjamin PETEL <benjamin.petel@outlook.com>
2021-04-13 23:01:48 +0200

    Correction du mail

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// -------------------- Nav Menu ---------------------
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelector(".nav-link");
@@ -12,4 +13,19 @@ function mobileMenu(){
 function closeMenu(){
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+
+
+// -------------------- Mail ---------------------
+const prenom = 'benjamin';
+const nom = 'petel';
+const client = '@outlook.com';
+const mail = document.querySelectorAll(".button_mail");
+
+for(let i=0;i<mail.length; i++){
+    mail[i].addEventListener("click", MailMe);
+}
+
+function MailMe(){
+    location.href = 'mailto:'+prenom+nom+client;
 }
