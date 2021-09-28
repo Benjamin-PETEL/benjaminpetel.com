11dd56ebdf0f0635f8c8e6fea0871322ff3e5bc9
Benjamin PETEL <benjamin.petel@outlook.com>
2021-09-28 21:27:29 +0200

    refactor: delete competence.js and clean up code in script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function mobileMenu(){
     navMenu.classList.toggle("active");
 }
 
-// -------------------- Mail ---------------------
+// ---------------------- Mail -----------------------
 const prenom = 'benjamin';
 const nom = 'petel';
 const client = '@outlook.com';
@@ -25,4 +25,39 @@ for(let i=0;i<mail.length; i++){
 
 function MailMe(){
     location.href = 'mailto:'+prenom+'.'+nom+client;
+}
+
+// --------------------- Skills ----------------------
+const selection = document.querySelector("#selection");
+const skillsButtons = selection.querySelectorAll(".btn");
+const skillsSection = document.querySelector("#skills");
+const skillsArticles = skillsSection.querySelectorAll("article");
+
+skillsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const skill = button.id.split("_")[0];
+        displayCompetence(skill);
+    });
+});
+
+
+function displayCompetence(competence){
+
+    skillsButtons.forEach((button) => {
+        const skill = button.id.split("_")[0];
+        if (skill === competence) button.classList.add("btnSelected");
+        else button.classList.remove("btnSelected");
+    });
+
+    skillsArticles.forEach((article) => {
+        console.log(article);
+        const skill = article.id.split("_")[0];
+        console.log(skill);
+        console.log(competence);
+        if (skill === competence){
+            console.log("block");
+            article.style.display = "block";
+        }
+        else article.style.display = "none";
+    });
 }
