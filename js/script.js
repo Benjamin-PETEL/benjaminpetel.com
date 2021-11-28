debc8299e361bf0d884d79b548196eed0c583dc5
Benjamin-PETEL <78384990+Benjamin-PETEL@users.noreply.github.com>
2021-11-28 22:22:08 +0100

    Add project section (#4)
    * add projects section
    
    * colors as variables
    
    * add caroussel to project section
    
    * add modals for projects and values
    
    * add responsive to project modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// -------------------- Nav Menu ---------------------
+// -------------------- NAV MENU ---------------------
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
@@ -13,7 +13,7 @@ function mobileMenu(){
     navMenu.classList.toggle("active");
 }
 
-// ---------------------- Mail -----------------------
+// ---------------------- MAIL -----------------------
 const prenom = 'benjamin';
 const nom = 'petel';
 const client = '@outlook.com';
@@ -27,7 +27,7 @@ function MailMe(){
     location.href = 'mailto:'+prenom+'.'+nom+client;
 }
 
-// --------------------- Skills ----------------------
+// --------------------- SKILLS ----------------------
 const selection = document.querySelector("#selection");
 const skillsButtons = selection.querySelectorAll(".btn");
 const skillsSection = document.querySelector("#skills");
@@ -58,31 +58,81 @@ function displayCompetence(competence){
     });
 }
 
-// --------------------- Values ----------------------
+// -------------------- PROJECTS ---------------------
+const items = document.querySelectorAll(".carousel article");
+setInitialClasses();
+let slide = 0;
+
+// Set event listeners
+document.querySelector("#carousel_button_next").addEventListener('click', ()=> {
+    move('next');
+});
+document.querySelector("#carousel_button_prev").addEventListener('click', ()=>{
+    move('prev')
+});
+
+function setInitialClasses(){
+    items[items.length - 1].classList.add("prev");
+    items[0].classList.add("active");
+    items[1].classList.add("next");
+    items[0].addEventListener('click', displayModal)
+}
+
+function move(direction){
+    if (direction==='next') slide++;
+    else slide--;
+
+    if (slide<0) slide=items.length -1;
+    if (slide>=items.length) slide=0;
+
+    moveCarouselTo();
+}
+
+function moveCarouselTo(){
+    // remove class active/next/previous from all elements
+    items.forEach(item =>{
+        item.classList.remove("active");
+        item.classList.remove("prev");
+        item.classList.remove("next");
+        item.removeEventListener('click', displayModal);
+    });
+
+    // add class prev to the previous element
+    if(slide===0) items[items.length -1].classList.add("prev");
+    else items[slide-1].classList.add("prev");
+
+    // add the class active to the active element
+    items[slide].classList.add("active");
+    items[slide].addEventListener('click', displayModal)
+
+    // add class next to the next element
+    if(slide===items.length -1) items[0].classList.add("next");
+    else items[slide+1].classList.add("next");
+}
+
+// --------------------- VALUES ----------------------
 const values = document.querySelector("#values");
 const valuesBtn = values.querySelectorAll("li");
 const valuesArticles = values.querySelectorAll("article");
 
 valuesBtn.forEach((button) => {
-    button.addEventListener("click", () => {
-        const value = button.id.split("_")[0];
-        displayValue(value);
-    });
+    button.addEventListener("click", displayModal);
 });
 
-function displayValue(value){
-    valuesArticles.forEach((article) => {
-        const articleValue = article.id.split("_")[0];
-        if (value === articleValue) article.style.display = "block";
-    });
+// --------------------- MODALS ----------------------
+const modalBackground = document.querySelector(".modalBackground");
+const modalDisplayBtns = document.querySelectorAll(".modalDisplayBtn");
+
+function displayModal(){
+    articleName = this.id.replace('Btn', 'Article');
+    modalBackground.classList.add("modalBackgroundDisplay");
+    modalBackground.querySelector("#" + articleName).classList.add('modalDisplay');
 }
 
 // close the modal if click outside the modal
-window.onclick = ((event) => {
-    valuesArticles.forEach((article) => {
-        if(event.target === article){
-            article.style.display = "none";
-        }
-
-    });
+modalBackground.onclick = ((event) => {
+    if (event.target.classList.contains('modalBackground')){
+        modalBackground.classList.remove('modalBackgroundDisplay');
+        modalBackground.querySelector(".modalDisplay").classList.remove('modalDisplay');
+    }
 });
